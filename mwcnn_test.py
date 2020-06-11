c56a9ad42b2fe6c1ec9d308b52e1fd82d3259a05
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-06-11 10:47:23 +0200

    added test to check that all variables change

--- mwcnn_test.py
+++ mwcnn_test.py
@@ -41,3 +41,20 @@ def test_mwcnn_conference():
     shape = [1, 32, 32, 1]
     res = model(tf.zeros(shape))
     assert res.shape.as_list() == shape
+
+def test_mwcnn_change():
+    model = MWCNN(
+        n_filters_per_scale=[4, 8, 16],
+        n_convs_per_scale=[2, 2, 2],
+        n_first_convs=2,
+        first_conv_n_filters=4,
+    )
+    x = tf.random.normal((1, 64, 64, 1))
+    y = x
+    model(x)
+    before = [v.numpy() for v in model.trainable_variables]
+    model.compile(optimizer='sgd', loss='mse')
+    model.train_on_batch(x, y)
+    after = [v.numpy() for v in model.trainable_variables]
+    for b, a in zip(before, after):
+        assert np.any(np.not_equal(b, a))
